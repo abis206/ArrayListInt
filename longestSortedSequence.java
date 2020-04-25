5f8fe7b8b247bbdb2a91099d354dd38afb7a2882
Abas Hersi <55505133+abis206@users.noreply.github.com>
2020-04-25 14:48:12 -0700

    Create longestSortedSequence.java

---
+++ longestSortedSequence.java
@@ -0,0 +1,24 @@
+/**
+View Problem Here: https://practiceit.cs.washington.edu/problem/view/bjp4/chapter15/e10-longestSortedSequence
+*/ 
+
+public int longestSortedSequence() {
+    if (size == 0)
+        return 0;
+
+    int max = 1;
+    int length = 1;
+
+    for (int i = 1; i < size; i++) {
+        if (elementData[i - 1] <= elementData[i]) {
+            length++;
+        } else {
+            if (length > max) {
+                max = length;
+            }
+            length = 1;
+        }
+    }
+
+    return Math.max(max, length);
+}
